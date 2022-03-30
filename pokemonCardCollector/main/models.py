e7b6d20f2902f7ab46035b845e0fdbcd7151ea4f
Brandon Samaroo <samaroo.bran@gmail.com>
2022-03-30 16:43:56 -0400

    finished day 2

--- pokemonCardCollector/main/models.py
+++ pokemonCardCollector/main/models.py
@@ -1,5 +1,5 @@
-from pyexpat import model
 from django.db import models
+from django.urls import reverse
 
 class PokemonCard(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +8,18 @@ class PokemonCard(models.Model):
     rarity = models.IntegerField()
     condition = models.IntegerField()
     picture = models.ImageField(upload_to ='images')
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"card_id": self.id})
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        ordering = ['rarity']
+
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    site = models.URLField()
+    card = models.ForeignKey(PokemonCard, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.card} listing on {self.name} link: {self.site}" #get display is a built in function for displaying choices
+    class Meta:
+        ordering = ['name']
--- pokemonCardCollector/main/views.py
+++ pokemonCardCollector/main/views.py
@@ -1,7 +1,10 @@
 import imp
-from django.shortcuts import render
+from turtle import update
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import PokemonCard
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import ListingForm
 
 # Create your views here.
 
@@ -18,4 +21,25 @@ def cards_index(request):
 
 def card_detail(request, card_id):
     card =  PokemonCard.objects.get(id=card_id)
-    return render(request, "cards/detail.html", {'card': card})
+    listing_form = ListingForm()
+    return render(request, "cards/detail.html", {'card': card, 'listing_form': listing_form})
+
+class CardCreate(CreateView):
+    model = PokemonCard
+    fields = '__all__'
+
+class CardUpdate(UpdateView):
+    model = PokemonCard
+    fields = '__all__'
+
+class CardDelete(DeleteView):
+    model = PokemonCard
+    success_url = '/cards/'
+
+def add_listing(request, card_id):
+    form = ListingForm(request.POST)
+    if form.is_valid():
+        new_listing = form.save(commit=False)
+        new_listing.card_id = card_id
+        new_listing.save()
+    return redirect('detail', card_id = card_id)
